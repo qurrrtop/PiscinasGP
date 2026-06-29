1f4b6cf6c559f0568daaf30dfd615a8bd5296526
roman <romanmaidana757@gmail.com>
2026-06-29 19:48:19 -0300

    AppException

--- src/main/java/com/mycompany/piscinasgp/exceptions/PersistenceException.java
+++ src/main/java/com/mycompany/piscinasgp/exceptions/PersistenceException.java
@@ -1,13 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.mycompany.piscinasgp.exceptions;
 
-/**
- *
- * @author Usuario
- */
-public class PersistenceException {
+
+public class PersistenceException extends AppException {
     
+    public PersistenceException ( String message ) {
+        super(message);
+    }
+    
+    public PersistenceException( String message, Throwable cause ) {
+        super(message, cause);
+    }
 }
